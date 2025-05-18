51c445d424a0ddf2fab2d4048d2552f540707278
Mohamadreza Tonekaboni <mr.tonekaboni2001@gmail.com>
2025-05-18 16:30:26 +0330

    change 2.py

--- tamrin4.py
+++ tamrin4.py
@@ -2,3 +2,4 @@ def plus(a,b):
     return a + b
 
 print(plus(2,5))
+print(f"this is jam")
--- tamrin5.py
+++ tamrin5.py
@@ -2,3 +2,9 @@ def multy(c,d):
     return c * d
 
 print(multy(7,3))
+print(f"this is zarb")
+
+def get_name(name):
+    return f"hello {name}"
+
+print(get_name('ali'))
